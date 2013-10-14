5692ff30351b078c4e4904bec6e08ca6594bb1db
Nissav <nissav@uw.edu>
2013-10-14 11:24:42 -0700

    Update measureTemperature.c

--- mpLabProj4/measureTemperature.c
+++ mpLabProj4/measureTemperature.c
@@ -1,6 +1,21 @@
 void measureTemperature()
 {
+// send power on to measure circuit
 
+
+// wait until circuit is stable
+
+
+// perform measurement 
+
+
+// power off measureing circuit
+
+
+// convert to correct format 
+
+
+//write to SRAM 
 
 
 }
