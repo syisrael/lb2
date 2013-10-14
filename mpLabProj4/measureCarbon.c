a6c1d9ee6b4abdc392a84e2bcf3fd9f3dbcf171b
Nissav <nissav@uw.edu>
2013-10-14 14:06:21 -0700

    Update measureCarbon.c

--- mpLabProj4/measureCarbon.c
+++ mpLabProj4/measureCarbon.c
@@ -1,22 +1,59 @@
+
+// to put in initialzation
+/*
+MOVLB 0xF ; Set BSR for banked SFRs
+CLRF PORTA ; Initialize PORTA by
+; clearing output
+; data latches
+CLRF LATA ; Alternate method
+; to clear output
+; data latches
+MOVLW E0h ; Configure I/O 
+MOVWF ANSELA ; for digital inputs
+MOVLW 0CFh ; Value used to 
+; initialize data 
+; direction
+MOVWF TRISA ; Set RA<3:0> as inputs
+; RA<5:4> as outputs
+
+*/
+
 void measureCarbon()
 {
 // send power on to measure circuit
 
+//digital I/O to power controller initialzed as output
+RA0 = 1;
 
 // wait until circuit is stable
+//Delay by amount
 
 
-// perform measurement 
+//Perform Measurement
+// convert to correct format 
+//Choose a channel adc channel AN0
+ADCON0 = 0;
 
+//Turn on ADC
+ADON = 1;
+
+//Start conversion
+GODONE = 1;
+
+//Wait for converstion
+while(GODONE);
+
+//Turn off ADC
+ADON = 0;
+
+//Grab output
+carbonMeasure = ADRES;
 
 // power off measureing circuit
-
-
-// convert to correct format 
-
+RA0 = 0;
 
 //write to SRAM 
-
+writeSRAM() //attach carbonMeasure
 
 
 
@@ -24,3 +61,20 @@ void measureCarbon()
 
 
 }
+
+
+//Move to initialization section
+
+void ADCinit(){
+   /* ADCON2
+    * ADC Result Right Justified.
+    * Acquisition Time = 2TAD
+    * Conversion Clock = 32 Tosc
+    */
+
+   ADCON2=0b10001010;
+  
+  
+}
+
+
