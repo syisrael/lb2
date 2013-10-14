eb4f1951c2e634b0cb5392e62892f11ad07f9159
Nissav <nissav@uw.edu>
2013-10-14 11:24:16 -0700

    Update measureFlowRate.c

--- mpLabProj4/measureFlowRate.c
+++ mpLabProj4/measureFlowRate.c
@@ -1,5 +1,21 @@
 void measureFlowRate()
 {
 
+// send power on to measure circuit
+
+
+// wait until circuit is stable
+
+
+// perform measurement 
+
+
+// power off measureing circuit
+
+
+// convert to correct format 
+
+
+//write to SRAM 
 
 }
