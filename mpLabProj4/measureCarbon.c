f7618f934ce683faf79a3338cad83ada70a01574
Nissav <nissav@uw.edu>
2013-10-14 11:11:11 -0700

    Update measureCarbon.c

--- mpLabProj4/measureCarbon.c
+++ mpLabProj4/measureCarbon.c
@@ -1,5 +1,23 @@
 void measureCarbon()
 {
+// send power on to measure circuit
+
+
+// wait until circuit is stable
+
+
+// perform measurement 
+
+
+// power off measureing circuit
+
+
+// convert to correct format 
+
+
+//write to SRAM 
+
+
 
 
 
