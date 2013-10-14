0ee639b070315db819be5f7a59a3db33009c7114
Nissav <nissav@uw.edu>
2013-10-14 11:24:32 -0700

    Update measureSalinity.c

--- mpLabProj4/measureSalinity.c
+++ mpLabProj4/measureSalinity.c
@@ -1,6 +1,21 @@
 void measureSalinity()
 {
+// send power on to measure circuit
 
+
+// wait until circuit is stable
+
+
+// perform measurement 
+
+
+// power off measureing circuit
+
+
+// convert to correct format 
+
+
+//write to SRAM 
 
 
 }
