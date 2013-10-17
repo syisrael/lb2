f693f96e269330673627c0fcec10b56c034d647e
Jamin DeMattos <jaydemattos@gmail.com>
2013-10-16 19:19:49 -0700

    SRAM testing. Write seems to work. Read does not. Circuit may need pull-up resistors on DATA pins.

--- Good/main.c
+++ Good/main.c
@@ -15,21 +15,17 @@
 #pragma config WRTC=OFF             // Configuration Register Write Protection
 #pragma config PWRT=OFF              // Power up timer off
 
-typedef unsigned char byte;
 
-void delay() {
-    int d;
-    for(d = 0; d < 10000; d++);
-}
 
-#define     PIN_WE      PORTCbits.RC4
-#define     PIN_OE      PORTCbits.RC5
+#define     PIN_WE      PORTCbits.RC5
+#define     PIN_OE      PORTCbits.RC6
 #define     PIN_HZ      TRISD
 
 #define     PIN_A0      PORTCbits.RC0
 #define     PIN_A1      PORTCbits.RC1
 #define     PIN_A2      PORTCbits.RC2
 #define     PIN_A3      PORTCbits.RC3
+#define     PIN_A4      PORTCbits.RC4
 
 #define     PIN_D0      PORTDbits.RD0
 #define     PIN_D1      PORTDbits.RD1
@@ -42,7 +38,16 @@ void delay() {
 
 #define     SRAM_STORED_VALUES      16
 
-extern byte *SRAMAddressPtrs;
+typedef unsigned char byte;
+
+byte SRAMAddressPtrs[4] = { 0, 16, 32, 48 };
+void delay();
+
+
+void delay() {
+    int d;
+    for(d = 0; d < 100; d++);
+}
 
 void writeSRAM(byte selector, short data) {
     // Set address pins from stored value
@@ -50,48 +55,46 @@ void writeSRAM(byte selector, short data) {
     PIN_A1 = SRAMAddressPtrs[selector] >> 1 & 0b1;
     PIN_A2 = SRAMAddressPtrs[selector] >> 2 & 0b1;
     PIN_A3 = SRAMAddressPtrs[selector] >> 3 & 0b1;
+    PIN_A4 = SRAMAddressPtrs[selector] >> 4 & 0b1;
 
     // Set data pins on SRAM from a stored value in PIC memory
     PIN_D0 = data & 0b1;
-    PIN_D1 = data >> 1 & 0b1;
-    PIN_D2 = data >> 2 & 0b1;
-    PIN_D3 = data >> 3 & 0b1;
-    PIN_D4 = data >> 4 & 0b1;
-    PIN_D5 = data >> 5 & 0b1;
-    PIN_D6 = data >> 6 & 0b1;
-    PIN_D7 = data >> 7 & 0b1;
+    PIN_D1 = (data >> 1) & 0b1;
+    PIN_D2 = (data >> 2) & 0b1;
+    PIN_D3 = (data >> 3) & 0b1;
+    PIN_D4 = (data >> 4) & 0b1;
+    PIN_D5 = (data >> 5) & 0b1;
+    PIN_D6 = (data >> 6) & 0b1;
+    PIN_D7 = (data >> 7) & 0b1;
 
     // Complete write cycle on hardware SRAM
-    delay();
     PIN_OE = 0b1;
     PIN_WE = 0b0;
-    PIN_HZ = 0xf;
     delay();
     PIN_OE = 0b1;
     PIN_WE = 0b1;
-    PIN_HZ = 0xf;
     delay();
-    PIN_OE = 0b1;
-    PIN_WE = 0b1;
-    PIN_HZ = 0x0;
 
     // Increment SRAM pointer for measurement block
     SRAMAddressPtrs[selector] = (SRAMAddressPtrs[selector] + 1) % SRAM_STORED_VALUES + (selector * SRAM_STORED_VALUES);
 }
 
-short readSRAM(byte selector, byte history) {
+short readSRAM(byte selector, short history) {
     short data;
-
+    
     // Reads previous value
-    history = (SRAMAddressPtrs[selector] + SRAM_STORED_VALUES - history) % SRAM_STORED_VALUES + (selector * SRAM_STORED_VALUES);
+    history = (SRAMAddressPtrs[selector] + SRAM_STORED_VALUES - history - 1) % SRAM_STORED_VALUES + (selector * SRAM_STORED_VALUES);
 
     // Set address pins from stored value
     PIN_A0 = history & 0b1;
-    PIN_A1 = history >> 1 & 0b1;
-    PIN_A2 = history >> 2 & 0b1;
-    PIN_A3 = history >> 3 & 0b1;
+    PIN_A1 = (history >> 1) & 0b1;
+    PIN_A2 = (history >> 2) & 0b1;
+    PIN_A3 = (history >> 3) & 0b1;
+    PIN_A4 = (history >> 4) & 0b1;
 
     // Read data pins from SRAM and store value in PIC memory
+    PIN_HZ = 0xf;
+    PIN_OE = 0b0;
     delay();
     data = PIN_D7;
     data <<= 0b1;
@@ -108,31 +111,25 @@ short readSRAM(byte selector, byte history) {
     data |= PIN_D1;
     data <<= 0b1;
     data |= PIN_D0;
-
+    delay();
+    PIN_HZ = 0x0;
+    PIN_OE = 0b1;
+    delay();
+    
     return data;
 }
 
 void main() {
-    int i, b;
+    short i = 0, b = 0;
     TRISC = PORTC = TRISD = PORTD = 0x0; // Enable ports for digital output
-    
-    for (i = 0; i < 64; i++) {
+
+    /*for (i = 0; i < 16; i++) {
         writeSRAM(0, i);
-        writeSRAM(1, ~i);
-        writeSRAM(2, i ^ b);
-        writeSRAM(3, ~(i ^ b++));
-    }
+    }*/
     
     while(1) {
-        for (i = 0; i < 3; i++) {
-            readSRAM(i, 1);
-            readSRAM(i, 2);
-            readSRAM(i, 3);
-            readSRAM(i, 4);
-            readSRAM(i, 5);
-            readSRAM(i, 6);
-            readSRAM(i, 7);
-        }
+        for (i = 0; i < 16; writeSRAM(0, i++));
+        for (i = 0; i < 16; readSRAM(0, i++));
     }
 }
 /*
