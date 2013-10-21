373c42923f012d2806cc3d839bfa9500e3840cf1
Jamin DeMattos <jaydemattos@gmail.com>
2013-10-20 19:33:56 -0700

    asukdhakjhsdlkashdqasdasd

--- Good/build/default/production/main.o.d
+++ Good/build/default/production/main.o.d
@@ -1,3 +1,4 @@
 build/default/production/main.o:  \
-	C:/Users/jdema/Documents/git/lb2/Good/main.c  \
+	C:/Users/jayde_000/Dropbox/UW/EE478/lab2/git/lb2/Good/main.c  \
+	C:/Users/jayde_000/Dropbox/UW/EE478/lab2/git/lb2/Good/usart.h  \
 
--- Good/main.c
+++ Good/main.c
@@ -2,18 +2,22 @@
 //#include <p18f452.h>
 //#include <p18f25k22.h>
 #include <p18f452.h>
+#include "usart.h"
 
 //  turn off the watch dog timer
 #pragma config WDT=OFF              // Watchdog off
 #pragma config BOR=OFF              // Brown out reset
-#pragma config LVP=OFF               //
+#pragma config LVP=OFF              //
 #pragma config CP0=OFF              // Code protection
 #pragma config CP1=OFF              // Code protection
 #pragma config CP2=OFF              // Code protection
 #pragma config CP3=OFF              // Code protection
 #pragma config CPB=OFF              // Boot
 #pragma config WRTC=OFF             // Configuration Register Write Protection
-#pragma config PWRT=OFF              // Power up timer off
+#pragma config PWRT=OFF             // Power up timer off
+
+#define     PIN_TX      PORTCbits.RC7
+#define     PIN_RX      PORTCbits.RC6
 
 void delay() {
     int d;
@@ -21,5 +25,24 @@ void delay() {
 }
 
 void main() {
-    
+    short i = 0;
+    char str[] = "hello world";
+    OpenUSART(USART_TX_INT_OFF & 
+              USART_RX_INT_OFF & 
+              USART_ASYNCH_MODE & 
+              USART_NINE_BIT & 
+              USART_CONT_RX & 
+              USART_BRGH_HIGH & 
+              USART_ADDEN_OFF, 
+              12); 
+    putsUSART(str);
+    while (1) {
+        putsUSART("lo");
+        delay(1);
+        if (i % 10) {
+            putsUSART("\033[2J");
+            putsUSART(i);
+        }
+        i++;
+    }
 }
