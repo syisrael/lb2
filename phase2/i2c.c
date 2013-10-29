865347b745bd6517da094160f4718fc9f89a66e2
Jamin DeMattos <jaydemattos@gmail.com>
2013-10-28 18:02:25 -0700

    Pushing my work before Nissa takes my computer

--- phase2/i2c.c
+++ phase2/i2c.c
@@ -1,21 +1,11 @@
 #include <p18f452.h>
 #include <i2c.h>
-#include <referenceTimers.h>
 #include <ctype.h>
 #include <stdio.h>
-#include <referenceTimers.h>
+#include <timers.h>
 
 #define     MAX_RETRY_ATTEMPTS          10
-#define     MAX_WAITTIME                5       // seconds
-
-#define     MASTER_ADDRESS              0
-#define     SLAVE0_ADDRESS              1
-#define     SLAVE1_ADDRESS              2
-#define     SLAVE2_ADDRESS              3
-#define     SLAVE3_ADDRESS              4
-#define     SLAVE4_ADDRESS              5
-#define     SLAVE5_ADDRESS              6
-
+#define     MAX_WAITTIME                5   // seconds
 #define     DEVICE_ADDRESS              0   // MASTER_ADDRESS 0
                                             // SLAVE0_ADDRESS 1
                                             // SLAVE1_ADDRESS 2
@@ -23,13 +13,16 @@
                                             // SLAVE3_ADDRESS 4
                                             // SLAVE4_ADDRESS 5
                                             // SLAVE5_ADDRESS 6
-#define     I2C_CLOCK_DIVIDER           9   //
-                                        
+#define     I2C_CLOCK_DIVIDER           9   // 400 kHZ
+
+#define     TRANSMIT_PACKET_SIZE        20
+#define     RECEIVE_PACKET_SIZE         52
+
 // flagTXData
 // flagRXData
-// retryAttempts
-// errors
-// referenceTimer
+int retryAttempts = 0;
+int errors = 0;
+int referenceTimer = 0;
 
 #if !DEVICE_ADDRESS
 // MASTER
@@ -42,10 +35,12 @@ void setupI2C() {
     StartI2C();
 }
 
+char transmitBuffer[TRANSMIT_PACKET_SIZE];
+char receiveBuffer[RECEIVE_PACKET_SIZE];
 void communications() {
     switch (mState) {
     case M_IDLE:
-        if (flagTXData) {
+        if (transmitBuffer) {
             mState = M_TRANSMIT;
             retryAttempts = 0;
         }
@@ -57,14 +52,14 @@ void communications() {
         mState = M_WAITACK;
         break;
     case M_WAITACK:
-        if (/* receive ack */) { // Slave ACK
+        /*if (/* receive ack *) { // Slave ACK
             mState = M_LISTEN;
             retryAttempts = 0;
         } else if (retryAttempts > MAX_RETRY_ATTEMPTS) { // Limit exceeded
             mState = M_ERROR;
         } else if (referenceTimer > MAX_WAITTIME) { // Retransmit
             mState = M_TRANSMIT;
-        }
+        }*/
         break;
     case M_LISTEN:
         if (retryAttempts > MAX_RETRY_ATTEMPTS | referenceTimer > MAX_WAITTIME) { // Limit exceeded
---
+++ phase2/mainmaster.c
@@ -0,0 +1,48 @@
+#include <p18f452.h>
+#include "xlcd.h"
+#include "measure.h"
+#include <usart.h>
+#include <timers.h>
+#include <string.h>
+#include "lcd.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+#pragma config WDT=OFF              // Watchdog off
+#pragma config BOR=OFF              // Brown out reset
+#pragma config LVP=OFF              //
+#pragma config CP0=OFF              // Code protection
+#pragma config CP1=OFF              // Code protection
+#pragma config CP2=OFF              // Code protection
+#pragma config CP3=OFF              // Code protection
+#pragma config CPB=OFF              // Boot
+#pragma config WRTC=OFF             // Configuration Register Write Protection
+
+int tCounter = 0;
+
+#pragma code high_vector=0x08
+void high_interrupt (void)
+{
+	_asm GOTO timer_isr _endasm
+}
+#pragma code
+#pragma interrupt timer_isr
+
+void timer_isr (void)
+{
+	INTCONbits.TMR0IF = 0;
+	tCounter++;
+}
+
+void main(void)
+{
+    OpenTimer0( TIMER_INT_ON & T0_16BIT & T0_SOURCE_INT & T0_PS_1_128);
+    INTCONbits.GIE = 1; //enable interrupts
+    TRISBbits.RB1 = 1; //F or C switch
+    setupMeasurement();
+    setupLCD();
+    measureCheck();
+    while(1) {
+        communications();
+    }
+}
